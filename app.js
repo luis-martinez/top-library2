da415e2ed6a74dd1c7009e254bac1c67b93e5537
luis-martinez <luis-martinez@users.noreply.github.com>
2025-11-06 18:47:24 -0800

    Constructor Book, addBookLibreary

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("hola");
+const myLibrary = [];
+
+function Book(id, title, author, pages) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+
+function addBookLibrary(title, author, pages) {
+  let id = crypto.randomUUID();
+
+  let book = new Book(id, title, author, pages);
+
+  myLibrary.push(book);
+}
+
+let hobbit = addBookLibrary("The Hobbit", "JRR Tolkien", 300);
+let rings = addBookLibrary("The Lord of the Rings", "JRR Tolkien", 350);
+
+console.log(myLibrary);
