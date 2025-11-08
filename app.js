c2d7752e63acc73418f05fe046c6be2d35e698c6
luis-martinez <luis-martinez@users.noreply.github.com>
2025-11-08 13:21:27 -0800

    Add read status

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const myLibrary = [];
 
-function Book(id, title, author, pages) {
+function Book(id, title, author, pages, read) {
   this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
 
-function addBookLibrary(title, author, pages) {
+function addBookLibrary(title, author, pages, read) {
   let id = crypto.randomUUID();
 
-  let book = new Book(id, title, author, pages);
+  let book = new Book(id, title, author, pages, read);
 
   myLibrary.push(book);
 }
+
 
 function displayLibrary() {
   for (let i = 0; i < myLibrary.length; i++) {
@@ -23,6 +25,7 @@ function displayLibrary() {
   }
 }
 
+
 function displayBook(book) {
   const container = document.querySelector("#container");
   const tr = document.createElement("tr");
@@ -30,15 +33,16 @@ function displayBook(book) {
   tr.innerHTML = `<td>${book.id}</td>
                   <td>${book.title}</td>
                   <td>${book.author}</td>
-                  <td>${book.pages}</td>`;
+                  <td>${book.pages}</td>
+                  <td>${book.read}</td>`;
 
   container.appendChild(tr);
 }
 
 // MAIN
 
-let hobbit = addBookLibrary("The Hobbit", "JRR Tolkien", 300);
-let rings = addBookLibrary("The Lord of the Rings", "JRR Tolkien", 350);
+let hobbit = addBookLibrary("The Hobbit", "JRR Tolkien", 300, true);
+let rings = addBookLibrary("The Lord of the Rings", "JRR Tolkien", 350, false);
 
 // console.log(myLibrary);
 // console.table(myLibrary);
