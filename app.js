bee8c32dde860386465be623a5b6cd4c1f78d054
luis-martinez <luis-martinez@users.noreply.github.com>
2025-11-15 14:30:10 -0800

    Add new book

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function addBookLibrary(title, author, pages, read) {
   let book = new Book(id, title, author, pages, read);
 
   myLibrary.push(book);
+  return book;
 }
 
 
@@ -39,12 +40,64 @@ function displayBook(book) {
   container.appendChild(tr);
 }
 
+
+
 // MAIN
 
 let hobbit = addBookLibrary("The Hobbit", "JRR Tolkien", 300, true);
 let rings = addBookLibrary("The Lord of the Rings", "JRR Tolkien", 350, false);
 
 // console.log(myLibrary);
-// console.table(myLibrary);
+console.table(myLibrary);
 
 displayLibrary();
+
+
+// NEW BOOK
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+const form = document.querySelector("form");
+
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+// NEW BOOK SUBMIT
+
+const submit = document.querySelector("#add-book");
+
+submit.addEventListener("click", () => {
+  console.log("submit");
+  event.preventDefault();
+  // e.preventDefault();
+
+  // Get data from the dialog, new book
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const read = document.querySelector('input[name="read"]:checked');
+
+
+  console.log(title.value);
+  console.log(author.value);
+  console.log(pages.value);
+  console.log(read.value);
+
+
+  const newBook = addBookLibrary(title.value, author.value, pages.value, read.value);
+
+  // displayLibrary();
+  displayBook(newBook);
+console.table(myLibrary);
+
+  dialog.close();
+  form.reset();
+});
+
